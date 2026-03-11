75148d868be9e70f2f399bd3ffd81ebf4f627bd8
hcaroli381 <hcaroli381@g.educaand.es>
2026-03-11 08:24:06 +0100

    clase empleados

--- src/app/Empleados.java
+++ src/app/Empleados.java
@@ -1,5 +1,38 @@
 package app;
 
-public class Empleados {
+public abstract class Empleados {
+	private String dni;
+	private String nombre;
+	private String apellidos;
+	private double sueldoBase;
 
+	public Empleados(String dni, String nombre, String apellidos, double sueldoBase) {
+
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.sueldoBase = sueldoBase;
+	}
+
+	public double getsueldoBase() {
+		return sueldoBase;
+	}
+
+	public void setsueldoBase(double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public abstract double getSueldo();
 }
